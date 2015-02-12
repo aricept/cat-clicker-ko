42acc106a06ee1828634c1b679e9ad7cdbcefcd2
Nathan Miller <aricept@gmail.com>
2015-02-12 12:28:38 -0600

    Implement clickable CatList

--- js/app.js
+++ js/app.js
@@ -1,8 +1,55 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		nicknames: ['TabMan', 'T-Bone', 'Mr. T', 'Fat-T']
+	},
+	{
+		clickCount: 0,
+		name: 'Shadow',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		nicknames: ['Scooby','Punxatawney']
+	},
+	{
+		clickCount: 0,
+		name: 'Mischief',
+		imgSrc: 'img/4154543904_6e2428c421_z',
+		nicknames: ['Scratchy']
+	},
+	{
+		clickCount: 0,
+		name: 'Tom',
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		nicknames: ['Hunter']
+	}
+];
+
 var viewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.nicknames = ko.observableArray(['TabMan', 'T-Bone', 'Mr. T', 'Fat-T']);
+	var self = this;
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+
+	this.changeCat = function() {
+		self.currentCat(this);
+	}
+
+};
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames);
 	this.level = ko.computed(function() {
 		var clicks = this.clickCount();
 		if (clicks < 10) {
@@ -18,10 +65,6 @@ var viewModel = function() {
 			return 'Teen';
 		}
 	}, this);
-
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
 };
 
 ko.applyBindings(new viewModel());
